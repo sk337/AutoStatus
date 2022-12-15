acc1cfd7f104bd59ba0eddc9251ef8e72c94f9a9
nathan <nathan@shorthoppers.com>
2022-12-15 19:28:45 +0000

    Updated readme & Finshed auto status

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Auto Status
 
-## Replit hosting 
+AutoStatus is a program to sync your bio to you leaderboars posion
+
+## Getting an API key
+
+ask [@codergautam](https://forum.codergautam.dev/u/codergautam) for your an api key for auto status when he gives it to you keep it in a safe place because he can not get it again
+
+## Usage
+
+### Replit
+
+1. First you need to go to main.py and fill in all the varibles.
+
+2. then go into the secrets tab make a new secret call apiKey and pase your apikey in the value
+
+3. then press run
+
+## other services
+
+1. follow step 1 from repit
+
+2. put your api key from the forum  into the .env file after of `apiKey=`
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import time
 import requests
 import re
 
-Username = "ipaddy" # Discourse Username Here
-IGN = "ip" # Swordbattle in game name Here
-bio = "Test bio #%place%" # user %place% as a placeholder
+
+Username = "" # Discourse Username Here
+IGN = "" # Swordbattle in game name Here
+bio = "Basic bio #%place%" # user %place% as a placeholder
+
 
 def isReplit():
     try:
@@ -15,19 +17,19 @@ def isReplit():
         return False
 
 if isReplit():
-    os.getenv("apiKey") # Discourse Userapikey here
+    apiKey = os.getenv("apiKey") # Discourse Userapikey here
 else:
     from dotenv import load_dotenv
     load_dotenv()
-    os.getenv("apiKey")
+    apiKey = os.getenv("apiKey")
 
-
-
-#while True:
-if True:
+while True:
     req = requests.get(f"https://swordbattle.io/{IGN}")
-    place= re.findall('#.*all', req.text)[0].split(" ")[0].replace("#", '')
+    place = re.findall('#.*all', req.text)[0].split(" ")[0].replace("#", '')
     fullBio = bio.replace('%place%', place)
     print(f"setting @{Username}'s bio to \n```\n{fullBio}\n```")
-    # time.sleep(60*5)
-    # exit()
+    req2 = requests.put(f"https://forum.codergautam.dev/users/{Username}.json", data={"bio_raw": fullBio}, headers={"Api-Key": apiKey, "Api-Username": Username})
+    obj = req2.json()
+    if (obj['success']) & (obj['user']['bio_raw'] == fullBio):
+        print(f"bio succuess fully changed to {fullBio}")
+    time.sleep(60*5)
